1e80b853879139e694f823813674efc2c63c2f29
GODPLEASEENO <xoxlova2016@bk.ru>
2026-04-10 21:03:30 +0300

    tabs done

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+const tabNavigations = Array.from(document.querySelectorAll('.tab__navigation'));
+
+tabNavigations.forEach(navigation => {
+  const tabs = Array.from(navigation.querySelectorAll('.tab'));
+  const contents = Array.from(navigation.nextElementSibling.querySelectorAll('.tab__content'));
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(item => item.classList.remove('tab_active'));
+      contents.forEach(item => item.classList.remove('tab__content_active'));
+
+      tab.classList.add('tab_active');
+      contents[index].classList.add('tab__content_active');
+    });
+  });
+});
